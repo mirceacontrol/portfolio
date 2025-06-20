0cc5598574dd67723afc57cc2bec695c5d3d3979
mirceacontrol <ignatiucmircea112@gmail.com>
2025-06-20 22:33:30 +0300

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-200 to-pink-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-900">
+        Setup ok
+      </h1>
+    </div>
+  )
+}
+export default App
